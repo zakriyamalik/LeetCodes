38e2b2eafa3bf1d54eefab5425abfd7834052851
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-01-04 20:06:07 +0500

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    Example 2:
    
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    Example 3:
    
    Input: nums = [3,3], target = 6
    Output: [0,1]
    
    Constraints:
    
    2 <= nums.length <= 104
    -109 <= nums[i] <= 109
    -109 <= target <= 109
    Only one valid answer exists.

---
+++ Sum.java
@@ -0,0 +1,30 @@
+public class Sum {
+        public int[] twoSum(int[] nums, int target) {
+            // LinkedList<Integer> res = new LinkedList<Integer>();
+            int[] res = new int[2];
+
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = 0; j < nums.length; j++) {
+                    if (nums[i] == nums[j]&& i==j) {
+                    } else {
+                        if (nums[i] + nums[j] == target) {
+                            res[0] = i;
+                            res[1] = j;
+
+                            break;
+                        }
+                    }
+
+                }
+            }
+            return res;
+        }
+
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = sum.twoSum(nums, target);
+        System.out.println("The two indexes are: " + result[0] + " and " + result[1]);
+    }
+}
