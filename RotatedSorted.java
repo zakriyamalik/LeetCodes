555dae6602ea4ff06da3b906e7fa7432177e9fb4
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-01-07 21:51:18 +0500

    Search in RotatedSorted array There is an integer array nums sorted in ascending order (with distinct values).
    Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
    
    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
    
    You must write an algorithm with O(log n) runtime complexity.
    
    Example 1:
    
    Input: nums = [4,5,6,7,0,1,2], target = 0
    Output: 4
    Example 2:
    
    Input: nums = [4,5,6,7,0,1,2], target = 3
    Output: -1
    Example 3:
    
    Input: nums = [1], target = 0
    Output: -1
    
    Constraints:
    
    1 <= nums.length <= 5000
    -104 <= nums[i] <= 104
    All values of nums are unique.
    nums is an ascending array that is possibly rotated.
    -104 <= target <= 104

---
+++ RotatedSorted.java
@@ -0,0 +1,20 @@
+class RotatedSorted {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        RotatedSorted rotatedSorted = new RotatedSorted();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};//can rotate
+        int target = 0;
+        int result = rotatedSorted.search(nums, target);
+        System.out.println("The target is found at index: " + result);
+    }
+}
