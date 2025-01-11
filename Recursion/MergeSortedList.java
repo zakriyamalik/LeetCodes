ed6b30d5a07f085fceae4da4cb7185f4002ab119
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-01-11 22:35:14 +0500

    Merge 2 Sorted Lists You are given the heads of two sorted linked lists list1 and list2.
    Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
    
    Return the head of the merged linked list.
    
    Example 1:
    
    Input: list1 = [1,2,4], list2 = [1,3,4]
    Output: [1,1,2,3,4,4]
    Example 2:
    
    Input: list1 = [], list2 = []
    Output: []
    Example 3:
    
    Input: list1 = [], list2 = [0]
    Output: [0]
    
    Constraints:
    
    The number of nodes in both lists is in the range [0, 50].
    -100 <= Node.val <= 100
    Both list1 and list2 are sorted in non-decreasing order.

---
+++ Recursion/MergeSortedList.java
@@ -0,0 +1,55 @@
+package Recursion;
+
+
+class MergeSortedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if(list1==null)
+        {
+            return list2;
+        }
+        if(list2==null)
+        {
+            return list1;
+        }
+        if(list1.val<list2.val)
+        {
+
+           list1.next=mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else
+        {
+
+            list2.next=mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+
+    }
+    public void main(String[] args) {
+        MergeSortedList mergeSortedList = new MergeSortedList();
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode mergedList = mergeSortedList.mergeTwoLists(list1, list2);
+
+        while(mergedList!=null)
+        {
+            System.out.print(mergedList.val+" ");
+            mergedList=mergedList.next;
+        }
+    }
+}
