96f968232f0b8cb87d27b47f6e3bf8f1c6ca7d2c
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-01-17 19:15:20 +0500

    Still Trying ........

--- Recursion/good2.java
+++ Recursion/good2.java
@@ -4,97 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class good2 {
-    int count=0;
-    int even=-2;
-    int prime=1;
+    int count = 0;
+    int even = -2;
+    int prime = 1;
+
     public int countGoodNumbers(long n) {
-        List<Integer> list=new ArrayList<>();
-        List<List<Integer>> combos=new ArrayList<>();
-        List<Integer> sub=new ArrayList<>();
-        return getEven(combos,list,n).size();
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> combos = new ArrayList<>();
+        List<Integer> sub = new ArrayList<>();
+        return getEven(combos, list, n).size();
     }
 
-    private List<List<Integer>> getEven(List<List<Integer>> combos, List<Integer> list,long n) {
-        if(generateEven(combos,list).getLast().size()<n)
-        {
-            generateEven(combos,list);
+    private List<List<Integer>> getEven(List<List<Integer>> combos, List<Integer> list, long n) {
+        int size = generateEven(combos, list).get(list.size() - 1).size();
+        if (size < n) {
+            getEven(combos, list, n);
         }
-        return generateEven(combos,list);
+        return generateEven(combos, list);
     }
 
-    private List<List<Integer>> generateEven(List<List<Integer>> combos,List<Integer> sub) {
-        if(even+2>8)
-        {
+    private List<List<Integer>> generateEven(List<List<Integer>> combos, List<Integer> sub) {
+        if (even + 2 > 8) {
             return combos;
         }
-        even=even+2;
-        sub.clear();
+        even = even + 2;
         sub.add(even);
-        generatePrime(combos,sub);
-        prime=0;
-
-        generateEven(combos,sub);
+        generatePrime(combos, sub);
+        generateEven(combos, sub);
         return combos;
     }
 
-    private List<List<Integer>> generatePrime(List<List<Integer>> combos,List<Integer> sub) {
-        if(prime>8)
-        {
+    private List<List<Integer>> generatePrime(List<List<Integer>> combos, List<Integer> sub) {
+        if (prime > 8) {
             return combos;
         }
-        while (!nextPrime(++prime) )
-        {
-            if (prime>=7)
-            {
-                break;
-            }
-        }
-        if(prime>7) {
-            return combos;
-        }
-        else {
-            sub.add(prime);
+        int[] primeDigits = {2, 3, 5, 7};
+        for (int primeDigit : primeDigits) {
+            sub.add(primeDigit);
             combos.add(new ArrayList<>(sub));
-           // System.out.println(count+++ "\n");
-            for(int c:sub)
-            {
-                System.out.println(c+" ");
-            }
-            System.out.println("\n");
-            sub.removeLast();
-            generatePrime(combos, sub);
-            return combos;
+            sub.remove(sub.size() - 1);
         }
+        return combos;
     }
 
-    private Boolean nextPrime(int prime) {
-        if (prime<=1)
-        {
-            return false;
-        }
-        if(prime==2) {
-            return true;
-        }
-        for(int i=2;i<=Math.sqrt(prime);i++)
-        {
-            if(prime%i==0)
-            {
-                return false;
-            }
-        }
-
-
-        return true;
-
-    }
-
-
-    public static void main()
-    {
-        good2 good2= new good2();
-        int n=good2.countGoodNumbers(2);
-
-        System.out.println("Numbers are \t"+n);
-
+    public static void main(String[] args) {
+        good2 goodNumbers = new good2();
+        int n = goodNumbers.countGoodNumbers(4);
+        System.out.println("Numbers are \t" + n);
     }
 }
