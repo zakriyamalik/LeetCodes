ab9d39aca9a20a4513162a42483ee2b62cb8da09
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-01-25 17:24:55 +0500

    993. Cousins in Binary Tree
    Given the root of a binary tree with unique values and the values of two different nodes of the tree x and y, return true if the nodes corresponding to the values x and y in the tree are cousins, or false otherwise.
    
    Two nodes of a binary tree are cousins if they have the same depth with different parents.
    
    Note that in a binary tree, the root node is at the depth 0, and children of each depth k node are at the depth k + 1.

--- Trees/isCousin.java
+++ Trees/isCousin.java
@@ -88,9 +88,53 @@ public class isCousin {
     Queue<TreeNode> queue=new LinkedList<>();
     boolean res;
     public boolean isCousins(TreeNode root, int x, int y) {
-        queue.add(root);
+        if(root==null)
+        {
+            return false;
+        }
+
+        TreeNode p1;
+        TreeNode p2;
+        p1=root.left;
+        p2=root.right;
+        if(p1==null||p2==null)
+        {
+            return false;
+        }
+        queue.add(p1);
+        queue.add(p2);
         queue.add(null);
+
         while(!queue.isEmpty()) {
+
+            TreeNode poped = queue.poll();
+            if(poped==null)
+            {
+                if(!queue.isEmpty())
+                {
+                    queue.add(null);
+                }
+            }
+            else {
+                if(poped.left!=null&&poped.right!=null)
+                {
+                    if(poped.left.val==x&&poped.right.val==y||poped.left.val==y&&poped.right.val==x)
+                    {
+                        return false;
+                    }
+                }
+
+                if (poped.left != null) {
+                    queue.add(poped.left);
+                }
+                if (poped.right != null) {
+                    queue.add(poped.right);
+                }
+            }
+            if(poped!=null)
+            {
+                System.out.println("poped element is " + poped.val);
+            }
             count=0;
             for(TreeNode element:queue)
             {
@@ -106,27 +150,6 @@ public class isCousin {
             if(count==2)
             {
                 return true;
-            }
-            TreeNode poped = queue.poll();
-            if(poped==null)
-            {
-                if(!queue.isEmpty())
-                {
-                    queue.add(null);
-                }
-            }
-            else {
-
-                if (poped.left != null) {
-                    queue.add(poped.left);
-                }
-                if (poped.right != null) {
-                    queue.add(poped.right);
-                }
-            }
-            if(poped!=null)
-            {
-                System.out.println("poped element is " + poped.val);
             }
         }
         return false;
@@ -170,14 +193,25 @@ public class isCousin {
 
         //input 4
 
+//        TreeNode root = new TreeNode(1);
+//        root.left = new TreeNode(2);
+//        root.right = new TreeNode(3);
+//        root.left.right = new TreeNode(4); // Right child of 2
+//
+//        // Given x = 2 and y = 3
+//        int x = 2;
+//        int y = 3;
+
+
+        //input 5
+
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
-        root.left.right = new TreeNode(4); // Right child of 2
-
-        // Given x = 2 and y = 3
-        int x = 2;
-        int y = 3;
+        root.right.left = new TreeNode(4); // Left child of 3
+        root.right.right = new TreeNode(5); // Right child of 3
+       int x=4;
+       int y=5;
 
 
 
