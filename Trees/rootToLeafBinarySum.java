6d2a60cc8c5a3284569554b71a9d5ef777a75278
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-01-28 22:08:12 +0500

    1022. Sum of Root To Leaf Binary Numbers
    You are given the root of a binary tree where each node has a value 0 or 1. Each root-to-leaf path represents a binary number starting with the most significant bit.
    
    For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
    For all leaves in the tree, consider the numbers represented by the path from the root to that leaf. Return the sum of these numbers.
    
    The test cases are generated so that the answer fits in a 32-bits integer.
    Example
    Input: root = [1,0,1,0,1,0,1]
    Output: 22
    Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22

---
+++ Trees/rootToLeafBinarySum.java
@@ -0,0 +1,77 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class rootToLeafBinarySum {
+    int left=0;
+    int right=0;
+    int res=0;
+    String eq="";
+    List<Integer> list =new ArrayList<>();
+
+    public int sumRootToLeaf(TreeNode root) {
+       check2(root);
+       return res;
+    }
+
+    private int check2(TreeNode root) {
+        if(root==null)
+        {
+            return 0;
+        }
+        list.add(root.val);
+        left= check2(root.left);
+
+
+        right=check2(root.right);
+        if(root.left==null&& root.right==null)
+        {
+            System.out.println(list);
+
+            for (Integer i:list)
+            {
+                System.out.println(i+" ");
+                eq+=i;
+            }
+            int decimal = Integer.parseInt(eq, 2);
+            eq="";
+
+            res+=decimal;
+
+        } list.removeLast();
+
+        return root.val;
+    }
+
+    public static void main() {
+//        TreeNode root = new TreeNode(1);
+//        root.left = new TreeNode(2);
+//        root.right = new TreeNode(3);
+//
+//        root.left.left = new TreeNode(4);
+//        root.left.right = new TreeNode(5);
+//
+//        root.right.left = new TreeNode(6);
+//        root.right.right = new TreeNode(7);
+
+
+        //input 2
+
+        TreeNode root = new TreeNode(1);
+
+        // Creating and linking the left subtree
+        root.left = new TreeNode(0);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(1);
+
+        // Creating and linking the right subtree
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(1);
+
+        rootToLeafBinarySum solver = new rootToLeafBinarySum();
+        int sum = solver.sumRootToLeaf(root);
+        System.out.println("Sum of all root-to-leaf paths: " + sum);
+    }
+}
