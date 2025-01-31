40cc615527d7f3af8d50657ee73c7339736d15c3
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-01-31 23:56:06 +0500

    to be continued

---
+++ Trees/sortedArrToBinary.java
@@ -0,0 +1,34 @@
+package Trees;
+
+public class sortedArrToBinary {
+    TreeNode root=new TreeNode();
+    public TreeNode sortedArrayToBST(int[] nums) {
+        for (int i=0;i<nums.length;i++)
+        {
+           root= check2(nums[i],root);
+        }
+        return root;
+    }
+
+    private TreeNode check2(int num, TreeNode root) {
+        if(root==null)
+        {
+            return new TreeNode(num);
+        }
+        if(num<root.val)
+        {
+            root.left=check2(num,root.left);
+        }
+        else
+        {
+            root.right=check2(num,root.right);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-10, -3, 0, 5, 9};
+        sortedArrToBinary tree = new sortedArrToBinary();
+        TreeNode root = tree.sortedArrayToBST(arr);
+    }
+}
