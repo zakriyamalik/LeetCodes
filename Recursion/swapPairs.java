95af33ba53cc82e927db3bd9a56b4d890823382e
Muhammad Zakriya <zakriyamalik852@gmail.com>
2025-02-04 00:28:34 +0500

    24. Swap Nodes in Pairs
    Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)

--- Recursion/swapPairs.java
+++ Recursion/swapPairs.java
@@ -49,33 +49,80 @@ package Recursion;
 //}
 
 
+//public class swapPairs {
+//    ListNode prev;
+//    ListNode dummy=new ListNode(0);
+//    public ListNode swapPairs(ListNode head) {
+//        dummy.next=head;
+//        return swapPairs1(head);
+//    }
+//    public ListNode swapPairs1(ListNode head) {
+//        if(head==null)
+//        {
+//            return null;
+//        }
+//        prev=head;
+//        head=prev.next;
+//        head=check2(head);
+//        swapPairs1(head);
+//
+//        return dummy.next;
+//    }
+//
+//    private ListNode check2(ListNode head) {
+//        prev.next=head.next;
+//        head.next=prev;
+//        dummy.next=head;
+//        prev=prev.next;
+//        return prev;
+//
+//    }
+//
+//
+//    public static void main() {
+//        // write respective main function
+//        // Example usage:
+//        ListNode node1 = new ListNode(1);
+//        ListNode node2 = new ListNode(2);
+//        ListNode node3 = new ListNode(3);
+//        ListNode node4 = new ListNode(4);
+//        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        swapPairs swapPairs = new swapPairs();
+//        ListNode result = swapPairs.swapPairs(node1);
+//        System.out.println(result.val); // Output: 2
+//        System.out.println(result.next.val); // Output: 1
+//        System.out.println(result.next.next.val); // Output: 4
+//        System.out.println(result.next.next.next.val); // Output: 3
+//
+//
+//    }
+//}
+
+
+
 public class swapPairs {
-    ListNode prev;
-    ListNode dummy=new ListNode(0);
     public ListNode swapPairs(ListNode head) {
-        dummy.next=head;
-        return swapPairs1(head);
-    }
-    public ListNode swapPairs1(ListNode head) {
-        if(head==null)
-        {
-            return null;
-        }
-        prev=head;
-        head=prev.next;
-        head=check2(head);
-        swapPairs1(head);
+            // Create a dummy node to simplify edge cases
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode prev = dummy;
 
-        return dummy.next;
-    }
+            while (head != null && head.next != null) {
+                ListNode firstNode = head;
+                ListNode secondNode = head.next;
 
-    private ListNode check2(ListNode head) {
-        prev.next=head.next;
-        head.next=prev;
-        dummy.next=head;
-        prev=prev.next;
-        return prev;
+                prev.next = secondNode;
+                firstNode.next = secondNode.next;
+                secondNode.next = firstNode;
 
+                prev = firstNode;
+                head = firstNode.next;
+            }
+
+            // Return the new head node
+            return dummy.next;
     }
 
 
